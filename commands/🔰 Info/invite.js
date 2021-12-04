c44e05ac341f620d95df2e7e4b2ef8e60d3a3c5b
MrMap0 <MrMap0794@gmail.com>
2021-12-04 16:52:37 +0100

    commands

---
+++ commands/🔰 Info/invite.js
@@ -0,0 +1,50 @@
+const {
+  MessageEmbed,
+  MessageButton,
+  MessageActionRow
+} = require("discord.js")
+const config = require(`${process.cwd()}/botconfig/config.json`);
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const {
+  handlemsg
+} = require(`${process.cwd()}/handlers/functions`)
+module.exports = {
+  name: "invite",
+  category: "🔰 Info",
+  aliases: ["add"],
+  usage: "invite",
+  description: "Gives you an Invite link for this Bot",
+  type: "bot",
+  run: async (client, message, args, cmduser, text, prefix, player, es, ls) => {
+    let user = message.mentions.users.first() || client.user;
+    if (user) {
+      if (!user.bot) return interaction.reply({
+        ephemeral: true,
+        content: "<:no:833101993668771842> You can't Invite a Normal user! **IT MUST BE A BOT**"
+      })
+      let button_invite = new MessageButton().setStyle('LINK').setLabel("Invite " + user.username).setURL(`https://discord.com/api/oauth2/authorize?client_id=${user.id}&permissions=8&scope=bot%20applications.commands`)
+      //array of all buttons
+      const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
+      message.reply({
+        embeds: [new MessageEmbed()
+          .setColor(ee.color)
+          .setTitle(`Invite: __**${user.tag}**__`)
+          .setDescription(`||[*Click here for an Invitelink without Slash Commands*](https://discord.com/api/oauth2/authorize?client_id=${user.id}&permissions=8&scope=bot)||`)
+          .setURL(`https://discord.com/api/oauth2/authorize?client_id=${user.id}&permissions=8&scope=bot%20applications.commands`)
+          .setFooter('RM music | RHComn', 'https://cdn.discordapp.com/avatars/915607167711711363/5f79e5d6572980e3bd6c8f70a62e8cf2.webp')
+        ],
+        components: allbuttons
+      });
+    }
+  }
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.dev
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
---
+++ commands/🔰 Info/support.js
@@ -0,0 +1,40 @@
+const {
+  MessageEmbed,
+  MessageActionRow
+} = require("discord.js");
+const config = require(`${process.cwd()}/botconfig/config.json`);
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const {
+  MessageButton
+} = require('discord.js')
+module.exports = {
+  name: "support",
+  category: "🔰 Info",
+  usage: "invite",
+  description: "Sends you the Support Server Link",
+  type: "bot",
+  run: async (client, message, args, cmduser, text, prefix, player, es, ls) => {
+    let button_public_invite = new MessageButton().setStyle('LINK')
+    const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
+    message.reply({
+      embeds: [new MessageEmbed()
+        .setColor(ee.color)
+        .setTitle(client.la[ls].cmds.info.support.title)
+        .setDescription(eval(client.la[ls]["cmds"]["info"]["support"]["variable1"]))
+        .setFooter('RM music | RHComn', 'https://cdn.discordapp.com/avatars/915607167711711363/5f79e5d6572980e3bd6c8f70a62e8cf2.webp')
+        .setURL("https://discord.com/api/oauth2/authorize?client_id=784364932149280778&permissions=8&scope=bot%20applications.commands")
+      ],
+      components: allbuttons
+    });
+  }
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.dev
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
